c1a4f52ea78f258fa6ecc4768fc3ee44542dcd98
dearmannawar <dearmannawar@gmail.com>
2023-06-26 05:12:36 +0300

    End of section 17

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b16c84989f34382697e6308b4c65325db1a153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7a3e3196270b83f845bbbd04b7e3c05d7ef35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
